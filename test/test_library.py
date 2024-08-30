01f9c443e663f6d0a22f2a6a83ebcbbab6320ae3
shahkaran30062003 <shahkaran3062003@gmail.com>
2024-08-31 01:09:54 +0530

    add test to add multiple books.

--- test/test_library.py
+++ test/test_library.py
@@ -5,9 +5,14 @@ from library import add_book
 class TestLibrary(unittest.TestCase):
 
     def test_add_book(self):
-        book = {'isbn': '123456789', 'title': 'Learn Python',
-                'author': 'xyz', 'year': 2024}
-        self.assertIsNotNone(add_book(book))
+        book_1 = {'isbn': '123456789', 'title': 'Learn Python',
+                  'author': 'xyz', 'year': 2024}
+
+        book_2 = {'isbn': '123456788', 'title': 'Learn Python 2',
+                  'author': 'xyz', 'year': 2024}
+
+        self.assertEqual(add_book(book_1), [book_1])
+        self.assertEqual(add_book(book_2), [book_1, book_2])
 
 
 if __name__ == '__main__':
