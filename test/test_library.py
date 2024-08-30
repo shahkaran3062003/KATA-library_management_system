654eb94b052d4adf7ffbe78a1909289c411cd7ba
shahkaran30062003 <shahkaran3062003@gmail.com>
2024-08-31 01:01:25 +0530

    modify 'test_add_book' test to pass book object to 'add_book' function.

--- test/test_library.py
+++ test/test_library.py
@@ -5,7 +5,9 @@ from library import add_book
 class TestLibrary(unittest.TestCase):
 
     def test_add_book(self):
-        self.assertIsNotNone(add_book())
+        book = {'isbn': '123456789', 'title': 'Learn Python',
+                'author': 'xyz', 'year': 2024}
+        self.assertIsNotNone(add_book(book))
 
 
 if __name__ == '__main__':
