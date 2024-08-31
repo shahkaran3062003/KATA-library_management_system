0ec74d276833077bd5e22942023e5e41a6cb4e8a
shahkaran30062003 <shahkaran3062003@gmail.com>
2024-08-31 18:27:42 +0530

    add test to cross check that our available_books method not including borrowed books and this test pass, so no need to add code in available_books method.

--- test/test_library.py
+++ test/test_library.py
@@ -66,9 +66,11 @@ class TestLibrary(unittest.TestCase):
         new_library.add_book(self.book_2)
 
         self.assertIsNotNone(self.myLibrary.available_books())
-
         self.assertEqual(new_library.available_books(),
                          [self.book_1, self.book_2])
+
+        new_library.borrow_book(self.book_1.isbn)
+        self.assertEqual(new_library.available_books(), [self.book_2])
 
 
 if __name__ == '__main__':
